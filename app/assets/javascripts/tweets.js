2697a7e4ad909e9c9c6eb4877c3cac05677fad23
eiji <eijimine@gmail.com>
2017-08-23 16:20:02 -0400

    html version complete

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function() {
+
+var button = document.querySelector('#new_tweet');
+
+button.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+  $.ajax({
+  url: this.getAttribute('action'),
+  method: this.getAttribute('method'),
+  data: $(this).serialize(),
+  dataType: 'html'
+}).done(function(data) {
+  var ul = document.querySelector('ul.tweets');
+  var span = document.createElement('span');
+  span.innerHTML = data;
+  ul.insertBefore(span, ul.childNodes[0]);
+});
+
+});
+});
